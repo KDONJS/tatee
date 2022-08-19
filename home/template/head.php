562a5f373aee188e7efaecdb925f1a40ea2d5931
KDONJS <u19213103@utp.edu.pe>
2022-08-19 15:33:57 -0500

    autor

--- home/template/head.php
+++ home/template/head.php
@@ -8,7 +8,7 @@
     <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="BEL&MA S.A.C CONSTRUCTORA">
-    <meta name="author" content="AuCreative">
+    <meta name="author" content="kdon">
     <meta name="keywords" content="BEL&MA S.A.C CONSTRUCTORA">
 
     <title><?php echo $nombrepagina; ?></title>
