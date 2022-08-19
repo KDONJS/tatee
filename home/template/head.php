cd9d40b36c5183439153c75748dbe543c1b4aa2d
KDONJS <u19213103@utp.edu.pe>
2022-08-19 15:19:26 -0500

    identidad del sitio

--- home/template/head.php
+++ home/template/head.php
@@ -7,9 +7,9 @@
 
     <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Tatee Theme Templates">
+    <meta name="description" content="BEL&MA S.A.C CONSTRUCTORA">
     <meta name="author" content="AuCreative">
-    <meta name="keywords" content="Tatee Theme Templates">
+    <meta name="keywords" content="BEL&MA S.A.C CONSTRUCTORA">
 
     <title><?php echo $nombrepagina; ?></title>
 
