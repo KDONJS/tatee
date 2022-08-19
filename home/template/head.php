46988ac869fae5a5d0d74cf278076b1d7c409824
KDONJS <u19213103@utp.edu.pe>
2022-08-19 10:33:14 -0500

    git ignore

--- home/template/head.php
+++ home/template/head.php
@@ -5,7 +5,7 @@
 
 <head>
 
-    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Tatee Theme Templates">
     <meta name="author" content="AuCreative">
